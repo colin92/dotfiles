ffe624115772b20e7108ed8243a4e56df6d883fb
Colin Meret <colinmeret@gmail.com>
2017-09-25 13:12:27 -0400

    Added pathogen install step

--- setup.sh
+++ setup.sh
@@ -21,6 +21,9 @@ ln -s ~/dotfiles/.bash_profile .bash_profile
 
 echo "Installing vim plugins..."
 
+cp -r ~/dotfiles/.vim ~/.vim
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 git clone https://github.com/Xuyuanp/nerdtree-git-plugin.git ~/.vim/bundle/nerdtree-git-plugin
 git clone https://github.com/scrooloose/nerdcommenter.gita ~/.vim/bundle/nerdcommenter

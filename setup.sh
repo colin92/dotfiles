ec654735f4a8baac6b1e45f6e8f44fc972027092
Colin Meret <colinmeret@gmail.com>
2017-09-25 13:18:06 -0400

    removed type, made setup.sh executable

--- setup.sh
+++ setup.sh
@@ -26,7 +26,7 @@ mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 git clone https://github.com/Xuyuanp/nerdtree-git-plugin.git ~/.vim/bundle/nerdtree-git-plugin
-git clone https://github.com/scrooloose/nerdcommenter.gita ~/.vim/bundle/nerdcommenter
+git clone https://github.com/scrooloose/nerdcommenter.git ~/.vim/bundle/nerdcommenter
 git clone https://github.com/flazz/vim-colorschemes.git ~/.vim/bundle/vim-colorschemes
 git clone https://github.com/chr4/nginx.vim.git ~/.vim/bundle/nginx.vim
 
